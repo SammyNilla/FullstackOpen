fe887b1a6fd675cf63d05b8fc15af98648ca7079
Samantha Bartlett <sammynilla@protonmail.com>
2020-08-27 16:12:18 -0400

    committing my completed 2.1 exercise.

---
+++ part2/2.1/index.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = ({course}) => <h1>{course}</h1>
+const Part = ({name, exercises}) => <p>{name} {exercises}</p>
+const Content = ({parts}) => {
+  return (
+    <div>
+      <Part name={parts[0].name} exercises={parts[0].exercises} />
+      <Part name={parts[1].name} exercises={parts[1].exercises} />
+      <Part name={parts[2].name} exercises={parts[2].exercises} />
+    </div>
+  )
+}
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  )
+};
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
