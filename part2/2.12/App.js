0ae3c996bbb0d4f3f0d5b7db0ee6aa9fc3539ec4
Samantha Bartlett <sammynilla@protonmail.com>
2020-09-02 18:30:26 -0400

    small adjustments to 2.12 because I didn't realize we also needed to add in detailed location information.

--- part2/2.12/App.js
+++ part2/2.12/App.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+import Search from './Component/Search';
+import CountryList from './Component/CountryList';
+import CountryDetailed from './Component/CountryDetailed';
+
 const App = () => {
   const [ countries, setCountries ] = useState([]);
 
@@ -8,7 +12,7 @@ const App = () => {
     axios
       .get('https://restcountries.eu/rest/v2/all')
       .then(response => {
-        console.log(response.data)
+        //console.log(response.data)
         setCountries(response.data);
       })
   }, []);
@@ -16,30 +20,32 @@ const App = () => {
   const [ searchQuery, setSearchQuery ] = useState('');
   const handleSearchQueryChange = (event) => setSearchQuery(event.target.value);
 
-  const filteredCountries = countries.filter(country => country.name.toLowerCase().includes(searchQuery));
-  const showResults = (filteredCountries.length <= 10) ? true : false;
+  const filteredCountries = countries.filter(country => 
+    country.name.toLowerCase().includes(searchQuery)
+  );
 
-  // These are temporary?
-  const tooManyResults = () => <div>Too many matches, specify another filter</div>;
-  const resultsToShow = () => {
-    return (
-      <div>
-        {filteredCountries.map(country =>
-          <div key={country.name}>{country.name}</div>
-        )}
-      </div>
-    )
-  };
+  const showCountryList = () =>
+  {
+    const countryCount = filteredCountries.length;
+    if (countryCount <= 10 && countryCount > 1) {
+      return <CountryList countries={filteredCountries} />;
+    } else if (countryCount > 10) {
+      return <div>Too many matches, specify another filter</div>;
+    }
+  }
+  const showCountryDetails = () =>
+  {
+    if (filteredCountries.length === 1) {
+      return <CountryDetailed country={filteredCountries[0]} />;
+    }
+  }
 
   return (
-    <div>
-      <div>
-        find countries <input value={searchQuery} onChange={handleSearchQueryChange} />
-        {showResults ? 
-          resultsToShow() : tooManyResults()
-        }
-      </div>
-    </div>
+    <>
+      <Search query={searchQuery} handleChange={handleSearchQueryChange} />
+      {showCountryList()}
+      {showCountryDetails()}
+    </>
   )
 };
 
