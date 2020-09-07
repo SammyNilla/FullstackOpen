311c9b948c54f92b92e2aa9b5d5566e985da03f9
Samantha Bartlett <sammynilla@protonmail.com>
2020-09-07 19:54:25 -0400

    trying to commit completed exercise 3.10, may need to remove repo stuff from directory because of submodule confusion.

---
+++ part3/3.10/frontend/src/App.js
@@ -0,0 +1,135 @@
+import React, { useState, useEffect } from 'react';
+
+import Filter from './Component/Filter';
+import PersonForm from './Component/PersonForm';
+import Persons from './Component/Persons';
+
+import PersonsService from './Services/Persons';
+
+import './index.css'
+
+const Notification = ({details}) => {
+  if (details.message === null) {
+    return null;
+  }
+
+  return (
+    <div className={details.type}>
+      {details.message}
+    </div>
+  )
+}
+
+const App = () => {
+  const [ persons, setPersons ] = useState([]);
+  const [ notifDetails, setNotifDetails ] = useState({message: null, type: 'notif'});
+
+  useEffect(() => {
+    PersonsService
+      .getAll()
+      .then(ret => {
+        setPersons(ret)
+      })
+  }, []);
+
+  const [ newFilter, setNewFilter ] = useState('');
+  const filteredPersons = persons.filter(person => person.name.toLowerCase().includes(newFilter));
+  const handleNewFilter = (event) => {
+    setNewFilter(event.target.value);
+  };
+
+  const [ newName, setNewName ] = useState('');
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const [ newNumber, setNewNumber ] = useState('');
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const personDetails = {name: newName, number: newNumber};
+    const person = persons.find(p => p.name === personDetails.name);
+    if (person) {
+      const updatedPerson = {...person, number: newNumber};
+      if (window.confirm(`${newName} is already added to the phonebook, replace the old number with a new one?`)) {
+        updatePerson(person.id, updatedPerson);
+      }
+      return;
+    }
+
+    PersonsService
+      .create(personDetails)
+      .then(ret => {
+        setPersons(persons.concat(ret))
+        setNewName('');
+        setNewNumber('');
+        const newNotif = {
+          message: `Added ${personDetails.name}.`,
+          type: 'notif'
+        };
+        setNotifDetails(newNotif);
+        setTimeout(() => {
+          setNotifDetails({message: null});
+        }, 3000);
+      });
+  };
+
+  const updatePerson = (id, person) => {
+    PersonsService
+      .update(id, person)
+      .then(ret => {
+        setPersons(persons.map(p => p.id !== id ? p : ret));
+        setNewName('');
+        setNewNumber('');
+        const newNotif = {
+          message: `${person.name} has updated their number.`,
+          type: 'notif'
+        };
+        setNotifDetails(newNotif);
+        setTimeout(() => {
+          setNotifDetails({message: null});
+        }, 3000);
+      })
+      .catch(err => {
+        setPersons(persons.filter(p => p.id !== id));
+        const newNotif = {
+          message: `Information of ${person.name} has already been removed from the server.`,
+          type: 'error'
+        };
+        setNotifDetails(newNotif);
+        setTimeout(() => {
+          setNotifDetails({message: null});
+        }, 3000);
+      });
+  };
+
+  const removePerson = (id) => {
+    const person = persons.filter(p => p.id === id)[0];
+    if (window.confirm(`Delete ${person.name} ?`)) {
+      PersonsService
+      .remove(id)
+      .then(ret => {
+        setPersons(persons.filter(p => p.id !== id))
+      });
+    }
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Notification details={notifDetails} />
+      <Filter filter={newFilter} handleChange={handleNewFilter} />
+      <h3>add a new</h3>
+      <PersonForm handleSubmit={addPerson} 
+                  inputValueName={newName} handleNameChange={handleNameChange} 
+                  inputValueNumber={newNumber} handleNumberChange={handleNumberChange} />
+      <h3>Numbers</h3>
+      <Persons persons={filteredPersons} handleClick={removePerson} />
+    </div>
+  )
+};
+
+export default App;
