9c8a2bd51248e006b2635d521d2fa734e91be666
Samantha Bartlett <sammynilla@protonmail.com>
2020-09-13 01:19:32 -0400

    [done] exercise 3.22

---
+++ part3/3.22/backend/build/precache-manifest.876c56f9ac7d4f17d3ec47780c88b6c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21016f7622686e71521bf4f3790b8d8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "230777f728a574fb77d1",
+    "url": "/static/css/main.4aba5375.chunk.css"
+  },
+  {
+    "revision": "58dc22db1d4ae004ba16",
+    "url": "/static/js/2.9331a116.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9331a116.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "230777f728a574fb77d1",
+    "url": "/static/js/main.936a5a4f.chunk.js"
+  },
+  {
+    "revision": "8cab146f387c6fa6faf4",
+    "url": "/static/js/runtime-main.03d328f6.js"
+  }
+]);
---
+++ part3/3.22/backend/mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>');
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0.sylek.mongodb.net/phonebook?retryWrites=true&w=majority`;
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+if (process.argv.length > 3) {
+  // add person to database
+  // usually i think we would sanitize these command line arguments
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4]
+  });
+
+  person.save().then(res => {
+    console.log(`added ${res.name} number ${res.number} to phonebook`);
+    mongoose.connection.close();
+  });
+} else {
+  // print phonebook recipients
+  Person.find({}).then(res => {
+    console.log('phonebook:');
+    res.forEach(p => {
+      console.log(p.name, p.number);
+    });
+    mongoose.connection.close();
+  });
+}
