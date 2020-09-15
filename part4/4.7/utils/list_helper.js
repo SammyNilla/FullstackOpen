1eeabd4b635f06c64f564fc4e82bd6a6ae11d2ed
Samantha Bartlett <sammynilla@protonmail.com>
2020-09-15 15:03:20 -0400

    [done] exercise 4.7. probably not the cleanest solution but it gets the job done for our data set

---
+++ part4/4.7/utils/list_helper.js
@@ -0,0 +1,66 @@
+const _ = require('lodash');
+
+const dummy = (blogs) => {
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+
+  const reducer = (sum, blog) => {
+    return sum + blog.likes;
+  }
+
+  return blogs.reduce(reducer, 0);
+};
+
+const favoriteBlog = (blogs) => {
+
+  const reducer = (prev, curr) => {
+    return prev.likes > curr.likes ? prev : curr
+  };
+
+  return blogs.reduce(reducer);
+};
+
+const mostBlogs = (blogs) => {
+  let curated = [];
+  _.forEach(blogs, (blog) => {
+    let aExists = curated.some(b => b.author === blog.author);
+    if (!aExists) {
+      curated.push({ author: blog.author, blogs: 1 });
+    } else {
+      let aIndex = _.findIndex(curated, b => b.author === blog.author);
+      curated[aIndex].blogs += 1;
+    }
+  });
+
+  const reducer = (prev, curr) => {
+    return prev.blogs > curr.blogs ? prev : curr;
+  }
+  
+  return curated.reduce(reducer);
+};
+
+const mostLikes = (blogs) => {
+  let curated = [];
+  _.forEach(blogs, (blog) => {
+    let aExists = curated.some(b => b.author === blog.author);
+    if (!aExists) {
+      curated.push({ author: blog.author, likes: blog.likes });
+    } else {
+      let aIndex = _.findIndex(curated, b => b.author === blog.author);
+      curated[aIndex].likes += blog.likes;
+    }
+  });
+
+  const reducer = (prev, curr) => {
+    return prev.likes > curr.likes ? prev : curr;
+  }
+  
+  return curated.reduce(reducer);
+};
+
+module.exports = {
+  dummy, totalLikes, favoriteBlog,
+  mostBlogs, mostLikes
+};
