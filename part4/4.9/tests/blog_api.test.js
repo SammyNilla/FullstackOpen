a1168b6c2504b3ef45d2a1372001752d21eb54a8
Samantha Bartlett <sammynilla@protonmail.com>
2020-09-17 16:06:52 -0400

    [done] exercise 4.9

---
+++ part4/4.9/tests/blog_api.test.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const helper = require('./test_helper');
+const app = require('../app');
+const api = supertest(app);
+
+const Blog = require('../models/blog');
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+
+  const blogObjects = helper.initialBlogs
+    .map(blog => new Blog(blog));
+  const promiseArray = blogObjects.map(blog => blog.save());
+  await Promise.all(promiseArray);
+});
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/);
+});
+
+test('blog has property id defined', async () => {
+  const response = await api.get('/api/blogs');
+  const ids = response.body.map(r => r.id);
+  expect(ids).toBeDefined();
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
