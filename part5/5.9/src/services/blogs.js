36928a6cb147ff8d6395c44c73125f14309923a4
Samantha Bartlett <sammynilla@protonmail.com>
2020-09-25 02:36:21 -0400

    [done] exercise 5.9

---
+++ part5/5.9/src/services/blogs.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+const baseUrl = '/api/blogs';
+
+// eslint-disable-next-line no-unused-vars
+let token = null;
+const setToken = (newToken) => {
+  token = `bearer ${newToken}`
+};
+
+const getAll = async () => {
+  const request = axios.get(baseUrl)
+  const response = await request;
+  return response.data;
+};
+
+const create = async (newBlog) => {
+  const config = {
+    headers: { Authorization: token },
+  }
+
+  const request = axios.post(baseUrl, newBlog, config);
+  const response = await request;
+  return response.data;
+};
+
+const update = async (id, newBlog) => {
+  // despite what exercise 5.8 says, the only data we should need are
+  // blog likes and id
+  const request = axios.put(`${baseUrl}/${id}`, newBlog);
+  const response = await request;
+  return response.data;
+};
+
+export default { getAll, create, update, setToken };
