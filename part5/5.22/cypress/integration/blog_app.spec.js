133b138e5339385c9f796adbfa41bdec473c38c1
Samantha Bartlett <sammynilla@protonmail.com>
2020-09-27 00:49:06 -0400

    [done] exercise 5.22

---
+++ part5/5.22/cypress/integration/blog_app.spec.js
@@ -0,0 +1,125 @@
+
+describe('Blog app', function () {
+  beforeEach(function () {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset');
+    cy.createUser({ name: 'Sammynilla', username: 'sammynilla', password: 'secret' });
+    cy.createUser({ name: 'test user', username: 'abcd', password: '1234' });
+    cy.visit('http://localhost:3000');
+  });
+
+  it('Login form is shown', function () {
+    cy.contains('login').click();
+  });
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#loginFormUsername').type('sammynilla');
+      cy.get('#loginFormPassword').type('secret');
+      cy.contains('login').click();
+
+      cy.contains('Sammynilla logged in');
+    });
+    it('fails with wrong credentials', function () {
+      cy.contains('login').click();
+      cy.get('.error')
+        .should('contain', 'wrong username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)');
+    });
+  });
+
+  describe('When logged in', function () {
+    beforeEach(function () {
+      cy.login({ username: 'sammynilla', password: 'secret' });
+    });
+
+    it('A blog can be created', function () {
+      cy.contains('create new blog').click();
+      cy.get('#blogFormTitle').type('test blog');
+      cy.get('#blogFormAuthor').type('Sammynilla');
+      cy.get('#blogFormUrl').type('http://localhost:3003');
+      cy.get('#blogFormSubmit').click();
+
+      cy.contains('test blog Sammynilla');
+    });
+
+    describe('and a blog exists', function () {
+      beforeEach(function () {
+        cy.createBlog({
+          title: 'test blog',
+          author: 'Sammynilla',
+          url: 'http://localhost:3003',
+        });
+      });
+
+      it('it can liked', function () {
+        // since theres only one blog atm, this should be fine atm.
+        cy.contains('view').click();
+        cy.contains('like').click();
+      });
+      it('it can be deleted by the user who created it', function () {
+        cy.contains('test blog Sammynilla').parent()
+          .find('button').contains('view')
+          .as('toggleDetails');
+        cy.get('@toggleDetails').click();
+
+        // verifying that the remove button isn't hidden (implies it can be deleted)
+        cy.get('.details')
+          .find('button').contains('remove')
+          .as('removeBlog');
+        cy.get('@removeBlog').should('not.have.css', 'display', 'none');
+      });
+      it('it cannot be deleted by a user who didn\'t create it', function () {
+        // log in as a different user
+        cy.login({ username: 'abcd', password: '1234' });
+
+        cy.contains('test blog Sammynilla').parent()
+          .find('button').contains('view')
+          .as('toggleDetails');
+        cy.get('@toggleDetails').click();
+
+        // verifying that the remove button is hidden (implies it cannot be deleted)
+        cy.get('.details')
+          .find('button').contains('remove')
+          .as('removeBlog');
+        cy.get('@removeBlog').should('have.css', 'display', 'none');
+      });
+    });
+
+    describe('and multiple blogs exists', function () {
+      beforeEach(function () {
+        cy.createBlog({
+          title: 'test blog',
+          author: 'Sammynilla',
+          url: 'http://localhost:3003',
+        });
+        cy.createBlog({
+          title: 'test blog 2',
+          author: 'Sammynilla',
+          url: 'http://localhost:3003',
+          likes: 1
+        });
+      });
+
+      it('blogs are ordered by most likes', function () {
+        // define both blogs
+        cy.contains('test blog Sammynilla').parent().as('blog1');
+        cy.contains('test blog 2 Sammynilla').parent().as('blog2');
+
+        cy.get('@blog1')
+          .find('button').contains('view')
+          .as('toggleDetails');
+        cy.get('@blog2')
+          .find('button').contains('view')
+          .as('toggleDetails2');
+        // display details of both blogs
+        cy.get('@toggleDetails').click();
+        cy.get('@toggleDetails2').click();
+
+        // verify that the first blog grabbed now contains 'test blog 2 Sammynilla'
+        cy.get('.blog')
+          .eq(0)
+          .contains('test blog 2 Sammynilla');
+      });
+    });
+  });
+});
